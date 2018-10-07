6fef4141145ad35fed600fc47275ef15d648fc42
serovoy <alexander.serovoy@gmail.com>
2018-10-07 14:40:27 +0300

    init

---
+++ Assets/ChunkIterationPerformance/InitializeChunkIterationWorld.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Entities;
+using UnityEngine;
+namespace alexnown.ChunkIterationPerformance
+{
+    public struct SharedComponent : ISharedComponentData
+    {
+        public int Value;
+    }
+
+    public struct FirstTag : IComponentData { }
+    public struct SecondTag : IComponentData { }
+
+    public struct RandomValue : IComponentData
+    {
+        public float Value;
+    }
+
+
+    public class InitializeChunkIterationWorld : MonoBehaviour
+    {
+        public int ChunksCount = 10000;
+        [Range(1, 1000000)]
+        public int EntitiesInChunk = 1;
+
+        private World _world;
+        private void Start()
+        {
+            _world = new World("ChunksIteration");
+            _world.CreateManager<SingleQueryChunkIteration>();
+            var em = _world.GetOrCreateManager<EntityManager>();
+            int elementsForType = ChunksCount / 3;
+            InitEntities(em, elementsForType, ComponentType.Create<FirstTag>());
+            InitEntities(em, elementsForType, ComponentType.Create<SecondTag>());
+            InitEntities(em, ChunksCount - 2 * elementsForType);
+            ScriptBehaviourUpdateOrder.UpdatePlayerLoop(World.AllWorlds.ToArray());
+        }
+
+        private void InitEntities(EntityManager em, int count, params ComponentType[] components)
+        {
+            var componentsList = new List<ComponentType>() { ComponentType.Create<SharedComponent>(), ComponentType.Create<RandomValue>() };
+            componentsList.AddRange(components);
+            var archetype = em.CreateArchetype(componentsList.ToArray());
+            for (int i = 0; i < count; i++)
+            {
+                var shared = new SharedComponent { Value = i };
+                var instance = em.CreateEntity(archetype);
+                em.SetSharedComponentData(instance, shared);
+                for (int j = 0; j < EntitiesInChunk - 1; j++)
+                {
+                    var copy = em.Instantiate(instance);
+                    em.SetComponentData(copy, new RandomValue { Value = Random.value });
+                }
+                em.SetComponentData(instance, new RandomValue { Value = Random.value });
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (!_world.IsCreated)
+            {
+                _world.Dispose();
+                ScriptBehaviourUpdateOrder.UpdatePlayerLoop(World.AllWorlds.ToArray());
+            }
+        }
+    }
+}
---
+++ Assets/ChunkIterationPerformance/SingleQueryChunkIteration.cs
@@ -0,0 +1,68 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+namespace alexnown.ChunkIterationPerformance
+{
+    [DisableAutoCreation]
+    public class SingleQueryChunkIteration : ComponentSystem
+    {
+        private EntityArchetypeQuery _query;
+        private ArchetypeChunkComponentType<FirstTag> _firstType;
+        private ArchetypeChunkComponentType<SecondTag> _secondType;
+        private ArchetypeChunkComponentType<RandomValue> _randomType;
+
+        protected override void OnCreateManager()
+        {
+            _query = new EntityArchetypeQuery
+            {
+                Any = new ComponentType[0],
+                All = new[] { ComponentType.Create<RandomValue>() },
+                None = new ComponentType[0]
+            };
+            _firstType = GetArchetypeChunkComponentType<FirstTag>(true);
+            _secondType = GetArchetypeChunkComponentType<SecondTag>(true);
+        }
+
+        protected override void OnUpdate()
+        {
+            var chunks = EntityManager.CreateArchetypeChunkArray(_query, Allocator.Persistent);
+            _randomType = GetArchetypeChunkComponentType<RandomValue>(true);
+
+            float noTagsSum = 0;
+            float firstTagSum = 0;
+            float secondTagSum = 0;
+            for (int i = 0; i < chunks.Length; i++)
+            {
+                var chunk = chunks[i];
+                var firstTag = chunk.Has(_firstType);
+                var secondTag = chunk.Has(_secondType);
+                var array = chunk.GetNativeArray(_randomType);
+
+                if (firstTag)
+                {
+                    firstTagSum += CalcSumValues(array);
+                }
+                if (secondTag)
+                {
+                    secondTagSum += CalcSumValues(array);
+                }
+                else if (!firstTag)
+                {
+                    noTagsSum += CalcSumValues(array);
+                }
+            }
+            chunks.Dispose();
+        }
+
+        private float CalcSumValues(NativeArray<RandomValue> array)
+        {
+            float sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i].Value;
+            }
+            return sum;
+        }
+
+    }
+}

77ef8da9798b818b93170d846cf8db4cb8316156
serovoy <alexander.serovoy@gmail.com>
2018-10-25 18:59:13 +0300

    add NativeVariable container and tests for it

---
+++ Assets/Scripts/NativeContainers/NativeVariable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace alexnown.NativeContainers
+{
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeContainer]
+    [NativeContainerSupportsDeallocateOnJobCompletion]
+    unsafe public struct NativeVariable<T> where T : struct
+    {
+        public Allocator Allocator { get; private set; }
+        public bool Disposed { get; private set; }
+
+        public T Value
+        {
+            get
+            {
+                CheckIsDisposedAndThrow();
+                return UnsafeUtility.ReadArrayElement<T>(_addr, 0);
+            }
+            set
+            {
+                CheckIsDisposedAndThrow();
+                UnsafeUtility.WriteArrayElement(_addr, 0, value);
+            }
+        }
+
+        [NativeDisableUnsafePtrRestriction]
+        private void* _addr;
+
+        public NativeVariable(Allocator allocator)
+        {
+            
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            if (!UnsafeUtility.IsBlittable<T>())
+            {
+                throw new InvalidOperationException($"{typeof(T)} used in NativeVariable<{typeof(T)}> must be blittable.");
+            }
+#endif
+            Disposed = false;
+            Allocator = allocator;
+            int size = UnsafeUtility.SizeOf<T>();
+            _addr = UnsafeUtility.Malloc(size, UnsafeUtility.AlignOf<T>(), allocator);
+            UnsafeUtility.MemClear(_addr, size);
+        }
+
+        [WriteAccessRequired]
+        public void Dispose()
+        {
+            if (Disposed) return;
+            Disposed = true;
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            if (!UnsafeUtility.IsValidAllocator(Allocator))
+                throw new InvalidOperationException("The NativeVariable can not be Disposed because it was not allocated with a valid allocator.");
+#endif
+            UnsafeUtility.Free(_addr, Allocator);
+            _addr = null;
+        }
+
+        [Conditional("ENABLE_UNITY_COLLECTIONS_CHECKS")]
+        private void CheckIsDisposedAndThrow()
+        {
+            if(Disposed) throw new InvalidOperationException("NativeVariable already disposed.");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/NativeContainers/NativeVariableTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using alexnown.NativeContainers;
+using NUnit.Framework;
+using Unity.Collections;
+using UnityEngine;
+using Assert = NUnit.Framework.Assert;
+
+namespace alexnown.Tests
+{
+    [TestFixture]
+    [NUnit.Framework.Category("NativeVariable")]
+    public class NativeVariableTests
+    {
+        [Test]
+        public void IntNativeVariable()
+        {
+            var intVariable = new NativeVariable<int>(Allocator.Persistent);
+            Assert.AreEqual(0, intVariable.Value);
+
+            int settedValue = -25;
+            intVariable.Value = settedValue;
+            Assert.AreEqual(settedValue, intVariable.Value);
+
+            intVariable.Dispose();
+        }
+
+        [Test]
+        public void BlittableStructNativeVariable()
+        {
+            var variable = new NativeVariable<BlittableStruct>(Allocator.Temp);
+            Assert.AreEqual(default(BlittableStruct), variable.Value);
+
+            var myStract = new BlittableStruct { Id = long.MaxValue, Price = 123.45f };
+            variable.Value = myStract;
+            Assert.AreEqual(myStract, variable.Value);
+
+            variable.Dispose();
+        }
+
+        [Test]
+        public void NotBlittableChecks()
+        {
+            Action initNotblittableVariable = () =>
+            {
+                var variable = new NativeVariable<NotBlittableStruct>(Allocator.TempJob);
+                variable.Dispose();
+            };
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            Assert.Throws<InvalidOperationException>(() => initNotblittableVariable.Invoke());
+#else
+            Assert.DoesNotThrow(()=> initNotblittableVariable.Invoke());
+#endif
+        }
+
+        [Test]
+        public void CheckDispose()
+        {
+            Assert.Throws<InvalidOperationException>(() => default(NativeVariable<int>).Dispose());
+            var variable = new NativeVariable<int>(Allocator.Temp);
+            Assert.False(variable.Disposed);
+            variable.Dispose();
+            Assert.True(variable.Disposed);
+            Assert.DoesNotThrow(() => variable.Dispose());
+        }
+
+        [Test]
+        public void GetSetValueAfterDispose()
+        {
+            var variable = new NativeVariable<int>(Allocator.Temp);
+            variable.Dispose();
+            Assert.Throws<InvalidOperationException>(() => variable.Value = 25);
+            Assert.Throws<InvalidOperationException>(() => { int myValue = variable.Value; });
+        }
+
+        struct BlittableStruct
+        {
+            public long Id;
+            public float Price;
+        }
+
+        struct NotBlittableStruct
+        {
+            public GameObject Go;
+        }
+    }
+}
